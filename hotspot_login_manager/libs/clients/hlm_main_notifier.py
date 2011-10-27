69ace7c57b014cb77a99da4a5b5b2b9710b20b12
syam44 <aks92@free.fr>
2011-10-27 02:06:13 +0200

    --notifier: auto-reconnect when daemon goes down

--- hotspot_login_manager/libs/clients/hlm_main_notifier.py
+++ hotspot_login_manager/libs/clients/hlm_main_notifier.py
@@ -13,10 +13,10 @@
 
 
 #-----------------------------------------------------------------------------
-import getpass
 import os
 import re
 import sys
+import time
 #
 from hotspot_login_manager.libs.core import hlm_application
 from hotspot_login_manager.libs.clients import hlm_notifierbackend
@@ -27,40 +27,54 @@ from hotspot_login_manager.libs.clients import hlm_clientsocket
 def main(args):
     if not hlm_notifierbackend.isAvailable():
         sys.exit(1)
-
+    # First connection yields an error if the daemon is not available.
     clientSocket = hlm_clientsocket.ClientSocket()
 
-    if __INFO__: logInfo('HLM notifier daemon is up and running.')
-    try:
-        clientSocket.write('notify')
-        #
-        # Regexes for parsing the notifications:
-        # If a notification starts with [...] then the "..." is supposed
-        # to be an icon that we will pass to notify-send.
-        # Icons are taken from the hotspot_login_manager/icons directory.
-        #
-        regex = re.compile('^\\[([^]/]+)\\] ')
-        #
+    while True:
+        if __INFO__: logInfo('HLM notifier daemon is up and running.')
+        try:
+            clientSocket.write('notify')
+            #
+            # Regexes for parsing the notifications:
+            # If a notification starts with [...] then the "..." is supposed
+            # to be an icon that we will pass to notify-send.
+            # Icons are taken from the hotspot_login_manager/icons directory.
+            #
+            regex = re.compile('^\\[([^]/]+)\\] ')
+            #
+            while True:
+                message = clientSocket.readMessage()
+                if message == '':
+                    break
+                # Default icon
+                icon = None
+                match = regex.search(message)
+                if match != None:
+                    # We found [icon], check it and adjust the message
+                    iconName = match.group(1)
+                    iconPath = hlm_application.getPath() + '/icons/' + iconName + '.png'
+                    if os.path.isfile(iconPath):
+                        icon = iconPath
+                        message = message[len(iconName)+3:]
+                # Send the notification to the end-user
+                hlm_notifierbackend.notify(message, icon)
+        finally:
+            clientSocket.close()
+
+        # Reconnect silently if the daemon went down
+        if __DEBUG__: logDebug('Daemon went down, trying to reconnect...')
         while True:
-            message = clientSocket.readMessage()
-            if message == '':
+            try:
+                time.sleep(1)
+                clientSocket = hlm_clientsocket.ClientSocket()
+                if __DEBUG__: logDebug('Daemon went up again.')
                 break
-            # Default icon
-            icon = None
-            match = regex.search(message)
-            if match != None:
-                # We found [icon], check it and adjust the message
-                iconName = match.group(1)
-                iconPath = hlm_application.getPath() + '/icons/' + iconName + '.png'
-                if os.path.isfile(iconPath):
-                    icon = iconPath
-                    message = message[len(iconName)+3:]
-            # Send the notification to the end-user
-            hlm_notifierbackend.notify(message, icon)
-    finally:
-        if __INFO__: logInfo('HLM notifier daemon is shutting down...')
-        clientSocket.close()
-        sys.exit(0)
+            except SystemExit:
+                raise
+            except KeyboardInterrupt:
+                raise
+            except BaseException:
+                pass
 
 
 #-----------------------------------------------------------------------------
